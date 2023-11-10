b444207565786c136caa7b29af834b610dd3d3c3
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-10 15:40:25 +0200

    squash!

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,20 +5,30 @@
 
 // tslint:disable:no-unsafe-any
 
-import * as cp from 'child_process';
-import * as gulp from 'gulp';
-import * as path from 'path';
-import { gulp_installAzureAccount, gulp_webpack } from 'vscode-azureextensiondev';
-import { gulp_installRestClient } from './gulp/gulp_installRestClient';
+import * as cp from "child_process";
+import * as gulp from "gulp";
+import * as path from "path";
+import {
+	gulp_installAzureAccount,
+	gulp_webpack,
+} from "vscode-azureextensiondev";
+import { gulp_installRestClient } from "./gulp/gulp_installRestClient";
 
 const env = process.env;
 
 function test(): cp.ChildProcess {
-    env.DEBUGTELEMETRY = '1';
-    env.CODE_TESTS_PATH = path.join(__dirname, 'dist/test');
-    return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
+	env.DEBUGTELEMETRY = "1";
+	env.CODE_TESTS_PATH = path.join(__dirname, "dist/test");
+	return cp.spawn("node", ["./node_modules/vscode/bin/test"], {
+		stdio: "inherit",
+		env,
+	});
 }
 
-exports['webpack-dev'] = () => gulp_webpack('development');
-exports['webpack-prod'] = () => gulp_webpack('production');
-exports.test = gulp.series(gulp_installAzureAccount, gulp_installRestClient, test);
+exports["webpack-dev"] = () => gulp_webpack("development");
+exports["webpack-prod"] = () => gulp_webpack("production");
+exports.test = gulp.series(
+	gulp_installAzureAccount,
+	gulp_installRestClient,
+	test
+);
